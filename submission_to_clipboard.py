6627731f1dd8c0ad52d4641f995b123023681014
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2014-07-01 21:40:59 +0200

    guard stuff with if __name__ == '__main__'

--- submission_to_clipboard.py
+++ submission_to_clipboard.py
@@ -2,25 +2,25 @@ import sys
 import subprocess
 import re
 
-
 def load_module(name):
     return '##### {} #####\n{}\n\n'.format(name, open(name).read())
 
-text = ''
-text += load_module('img_lib.py')
-text += load_module('target_partition.py')
-text += load_module('sol.py')
+if __name__ == '__main__':
+    text = ''
+    text += load_module('img_lib.py')
+    text += load_module('target_partition.py')
+    text += load_module('sol.py')
 
-text = re.sub(r'(from \w+ import \*)', r'#\1', text)
-text = text.replace('__main__', '__mian__')
+    text = re.sub(r'(from \w+ import \*)', r'#\1', text)
+    text = text.replace('__main__', '__mian__')
 
-if sys.platform == 'win32':
-    p = subprocess.Popen(
-        'clip', shell=True, stdin=subprocess.PIPE)
-else:
-    p = subprocess.Popen(
-        'xsel --clipboard --input', shell=True, stdin=subprocess.PIPE)
-p.communicate(text)
-ret = p.wait()
-assert ret == 0
-print('solution copied to clipboard')
+    if sys.platform == 'win32':
+        p = subprocess.Popen(
+            'clip', shell=True, stdin=subprocess.PIPE)
+    else:
+        p = subprocess.Popen(
+            'xsel --clipboard --input', shell=True, stdin=subprocess.PIPE)
+    p.communicate(text)
+    ret = p.wait()
+    assert ret == 0
+    print 'solution copied to clipboard'
